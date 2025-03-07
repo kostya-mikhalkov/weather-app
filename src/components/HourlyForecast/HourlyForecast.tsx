f6e0f84c4a446d295d592829c68820fd5f6a4789
unknown <kostya.mkhalkov@bk.ru>
2025-03-07 12:09:13 +0300

    feat: completed hourly forecast component

--- src/components/HourlyForecast/HourlyForecast.tsx
+++ src/components/HourlyForecast/HourlyForecast.tsx
@@ -1,14 +1,28 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store/store";
 import HourlyForecastElements from "./HourlyForecastElements/HourlyForecastElements";
 
 import './HourlyForecast.css';
 
 const HourlyForecast: React.FC = () => {
+    const meteoState = useSelector((state: RootState) => state.meteo);
+    const toggleState = useSelector((state: RootState) => state.weather.toggle);
+
+    const hours = [0, 1, 2, 3, 4];
+
     return (
-        <div className="hourlyForecast">
+        <div className={toggleState ? "hourlyForecast active_colorLight" : "hourlyForecast"}>
             <h3 className="hourlyForecast__title">Hourly Forecast:</h3>
             <div className="hourlyForecast__wrapper">
-              <HourlyForecastElements time="2025-03-05T15:00"/>
+              {hours.map((item, ind) => (
+                <HourlyForecastElements key={ind}
+                                        time={meteoState.hourlyTime[item]} 
+                                        weatherDescr={meteoState.hourlyWeatherCode[item]} 
+                                        temp={+meteoState.hourlyTemperature[item]}
+                                        windDirection={meteoState.hourlyWindDirection[item]}
+                                        windSpeed={meteoState.hourlyWindSpeed[item]}/>
+              ))}
             </div>
         </div>
     )
--- src/components/HourlyForecast/HourlyForecastElements/HourlyForecastElements.tsx
+++ src/components/HourlyForecast/HourlyForecastElements/HourlyForecastElements.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../store/store";
 import clearSky from '../../../img/png/clear-sky.png';
 import drizzle from '../../../img/png/drizzle.png';
 import fog from '../../../img/png/fog.png';
@@ -7,19 +9,52 @@ import rainShowers from '../../../img/png/rainShowers.png';
 import rain from '../../../img/png/rain.png';
 import snow from '../../../img/png/snow.png';
 import thunder from '../../../img/png/thunderstorm.png';
+import nav from '../../../img/svg/navigation.svg';
 
 interface hourlyForecastInterface {
     time: string;
+    weatherDescr: string;
+    temp: number;
+    windDirection: number;
+    windSpeed: number;
 }
 
-const HourlyForecastElements: React.FC<hourlyForecastInterface> = ({time}) => {
+const HourlyForecastElements: React.FC<hourlyForecastInterface> = ({time, weatherDescr, temp, windDirection, windSpeed}) => {
+    const toggleState = useSelector((state: RootState) => state.weather.toggle);
+
     const realTime = new Date(time);
     const min = realTime.getMinutes() === 0 ? "00" : realTime.getMinutes();
     const hour = realTime.getHours() === 0 ? "00" : realTime.getHours();
+
+    const myStyle: React.CSSProperties = {
+        transform: `rotate(${windDirection}deg)`
+    }
     
     return (
-        <div className="hourlyForecastElements">
-          <span className="hourlyForecastElements__time">{hour}:{min}</span>
+        <div className={toggleState ? "hourlyForecast__elements dark" : "hourlyForecast__elements light"}>
+          <span className="hourlyForecast__elements__time">{hour}:{min}</span>
+          <div className="hourlyForecast__elements__weather">
+            <img src={
+                        weatherDescr === "Clear Sky" ? clearSky :
+                        weatherDescr === "Partly cloudy" ? partyCloudy :
+                        weatherDescr === "Fog" ? fog :
+                        weatherDescr === "Drizzle" ? drizzle :
+                        weatherDescr === "Rain" ? rain :
+                        weatherDescr === "Snow" ? snow :
+                        weatherDescr === "Rain showers" ? rainShowers :
+                        weatherDescr === "Thunderstorm" ? thunder : ''
+                    } 
+                    alt={weatherDescr}
+                    className="hourlyForecast__elements__weather__img" />
+                <span className="hourlyForecast__elements__weather__temp">{Math.floor(temp) + '°C'}</span>
+          </div>
+          <div className="hourlyForecast__elements__nav">
+            <img src={nav} 
+                 alt="navigation"
+                 style={myStyle} 
+                 className="hourlyForecast__elements__nav__img"/>
+            <span className="hourlyForecast__elements__nav__speed">{windSpeed} km/h</span>    
+          </div>    
         </div>
     )
 }  
